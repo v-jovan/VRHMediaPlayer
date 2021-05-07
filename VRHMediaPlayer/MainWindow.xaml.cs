4b4e23e80e74bc7e8c0f1f8919b4e1c5164d1027
Jovan Vrhovac <mr.vrhovac@mail.ru>
2021-05-07 23:54:05 +0200

    Minor bug fixes.

--- VRHMediaPlayer/MainWindow.xaml.cs
+++ VRHMediaPlayer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ namespace VRHMediaPlayer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         private readonly DispatcherTimer timer;
